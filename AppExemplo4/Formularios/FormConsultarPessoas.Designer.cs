6dd15e4e968ad9f4d25fbf74b050f66e590381bf
Rahyan Ramos <rahyanramos.dev@gmail.com>
2023-09-21 22:15:54 -0400

    Iniciando Consulta de pessoas do Excel

--- AppExemplo4/Formularios/FormConsultarPessoas.Designer.cs
+++ AppExemplo4/Formularios/FormConsultarPessoas.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbNome = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(580, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CONSULTAR PESSOAS";
+            // 
+            // cbNome
+            // 
+            this.cbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbNome.FormattingEnabled = true;
+            this.cbNome.Location = new System.Drawing.Point(114, 144);
+            this.cbNome.Name = "cbNome";
+            this.cbNome.Size = new System.Drawing.Size(570, 28);
+            this.cbNome.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(110, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Selecione o nome:";
+            // 
+            // FormConsultarPessoas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbNome);
+            this.Controls.Add(this.label1);
+            this.Name = "FormConsultarPessoas";
             this.Text = "FormConsultarPessoas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbNome;
+        private System.Windows.Forms.Label label2;
     }
 }
--- AppExemplo4/Formularios/FormConsultarPessoas.cs
+++ AppExemplo4/Formularios/FormConsultarPessoas.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClosedXML.Excel;
 
 namespace AppExemplo4.Formularios
 {
@@ -15,6 +16,15 @@ namespace AppExemplo4.Formularios
         public FormConsultarPessoas()
         {
             InitializeComponent();
+
+            var pasta = new XLWorkbook("C:\\registros\\cadastro.xlsx");
+            var planilha = pasta.Worksheet(1);
+            var linhas = planilha.RowsUsed();
+
+            for (int i = 2; i < linhas.Count()+1; i++)
+            {
+
+            }
         }
     }
 }
